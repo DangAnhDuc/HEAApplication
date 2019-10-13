cda54297a6883b56811381b623b51690c8c099c8
hmduc <kamen888.md@gmail.com>
2019-10-14 02:08:40 +0700

    update side bar

--- app/src/main/java/com/example/heaapp/view/MainActivity.java
+++ app/src/main/java/com/example/heaapp/view/MainActivity.java
@@ -1,12 +1,16 @@
 package com.example.heaapp.view;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
@@ -25,12 +29,15 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
+    @BindView(R.id.activity_main_drawer)
+    DrawerLayout sideBarLayout;
+//    @BindView(R.id.toolbar)
+//    Toolbar toolbar;
     @BindView(R.id.view_pager)
     ViewPager viewPager;
     @BindView(R.id.tab_layout)
     TabLayout tabLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +45,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("");
+//        setSupportActionBar(toolbar);
+//        getSupportActionBar().setTitle("");
+//        getSupportActionBar().setHomeButtonEnabled(true);
 
         initView();
     }
@@ -55,8 +63,20 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setCurrentItem(1);
         tabLayout.setupWithViewPager(viewPager);
 
+        mDrawerToggle = new ActionBarDrawerToggle(this,sideBarLayout,R.string.toggle_open,R.string.toggle_close);
+        sideBarLayout.addDrawerListener(mDrawerToggle);
+        mDrawerToggle.syncState();
+
     }
 
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(mDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     class ViewPagerAdapter extends FragmentPagerAdapter {
 
