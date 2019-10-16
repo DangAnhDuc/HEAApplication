5d4bdf0e759f40e62d3cd52b67b450f0b6fc7b99
DangAnhDuc <anhduc.dang@my.jcu.edu.au>
2019-10-16 21:15:24 +0700

    Add fireabe and some config

---
+++ app/src/main/java/com/example/heaapp/model/Current.java
@@ -0,0 +1,31 @@
+package com.example.heaapp.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Current {
+
+    @SerializedName("weather")
+    @Expose
+    private Weather weather;
+    @SerializedName("pollution")
+    @Expose
+    private Pollution pollution;
+
+    public Weather getWeather() {
+        return weather;
+    }
+
+    public void setWeather(Weather weather) {
+        this.weather = weather;
+    }
+
+    public Pollution getPollution() {
+        return pollution;
+    }
+
+    public void setPollution(Pollution pollution) {
+        this.pollution = pollution;
+    }
+
+}
