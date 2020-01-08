c91c3f21620386af7c29e3237e5e49081cdf90dd
Huynh Minh Duc <kamen888.md@gmail.com>
2020-01-08 13:46:33 +0700

    Delete redundant files

--- app/src/main/java/com/example/heaapp/presenter/ExerciseImagePresenter.java
+++ app/src/main/java/com/example/heaapp/presenter/ExerciseImagePresenter.java
@@ -1,7 +1,7 @@
 package com.example.heaapp.presenter;
 
 import com.example.heaapp.base.BasePresenter;
-import com.example.heaapp.view.activity.ExerciseImageView;
+import com.example.heaapp.view.fragment.ExerciseImageView;
 
 public interface ExerciseImagePresenter extends BasePresenter<ExerciseImageView> {
     void getListImage();
--- app/src/main/java/com/example/heaapp/view/fragment/ExerciseWorkoutFragment.java
+++ app/src/main/java/com/example/heaapp/view/fragment/ExerciseWorkoutFragment.java
@@ -2,7 +2,6 @@ package com.example.heaapp.view.fragment;
 
 
 import android.app.AlertDialog;
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -21,7 +20,6 @@ import com.example.heaapp.model.workout.ExerciseInfo.ItemExercise;
 import com.example.heaapp.presenter.ExerciseWorkoutPresenter;
 import com.example.heaapp.presenter.ExerciseWorkoutPresenterImpl;
 import com.example.heaapp.ultis.ultis;
-import com.example.heaapp.view.activity.ExerciseInfoActivity;
 
 import java.util.ArrayList;
 import java.util.List;
